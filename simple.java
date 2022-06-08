116949ad11799ef086e8b5589c05250a9d538aad
starx46 <starx46@gmail.com>
2022-06-08 10:52:33 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 class Simple {
 public static void main(String args[]){
-    System.out.println("welcome to jenkins learning in scrum with git");
+    System.out.println("welcome to jenkins learning in scrum with git integration");
 
 }
 
